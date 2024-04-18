adfab5fe73e8e16f9ec57fe0ca0df04de006bede
Madhav286 <164131589+Madhav286@users.noreply.github.com>
2024-04-18 14:01:03 +0530

    Update Person.java

--- Person.java
+++ Person.java
@@ -2,23 +2,33 @@ package javabasics.OOPS;
 
 public class Person {
     
-        // Attributes
-        private String name;
-        private int age;
+    // Attributes
+    private String name;
+    private int age;
+    
+    // Constructor
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    // Getter methods
+    public String getName() {
+        return name;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    
+    // Main method to execute the program
+    public static void main(String[] args) {
+        // Create a Person object
+        Person person = new Person("John Doe", 30);
         
-        // Constructor
-        public Person(String name, int age) {
-            this.name = name;
-            this.age = age;
-        }
-        
-        // Getter methods
-        public String getName() {
-            return name;
-        }
-        
-        public int getAge() {
-            return age;
-        }
+        // Display person information
+        System.out.println("Person details:");
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
     }
 }
