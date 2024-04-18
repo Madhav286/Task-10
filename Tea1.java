f021a5d567b6efd991926da1ebd8df30c5f959cf
Madhav286 <164131589+Madhav286@users.noreply.github.com>
2024-04-18 14:08:46 +0530

    Update Tea1.java

--- Tea1.java
+++ Tea1.java
@@ -1,6 +1,76 @@
-public class Tea1 {
-    // BlackTea subclass
-public class BlackTea extends Tea {
+public class Tea {
+    // Data members
+    private boolean hasHotWater;
+    private boolean hasTeaLeaves;
+    private boolean hasMilk;
+    private boolean hasSugar;
+    
+    // Constructor
+    public Tea() {
+        // Default constructor initializes tea without any ingredients
+        this.hasHotWater = false;
+        this.hasTeaLeaves = false;
+        this.hasMilk = false;
+        this.hasSugar = false;
+    }
+    
+    // Method to prepare basic tea with hot water and tea leaves
+    public void prepareTea() {
+        if (!hasHotWater) {
+            System.out.println("Boiling water...");
+            hasHotWater = true;
+        }
+        if (!hasTeaLeaves) {
+            System.out.println("Adding tea leaves...");
+            hasTeaLeaves = true;
+        }
+        System.out.println("Basic tea is ready.");
+    }
+    
+    // Method to add milk to the basic tea
+    public void addMilk() {
+        if (hasHotWater && hasTeaLeaves && !hasMilk) {
+            System.out.println("Adding milk...");
+            hasMilk = true;
+            System.out.println("Tea with milk is ready.");
+        } else {
+            System.out.println("Cannot add milk. Prepare basic tea first.");
+        }
+    }
+    
+    // Method to add sugar to the basic tea
+    public void addSugar() {
+        if (hasHotWater && hasTeaLeaves && !hasSugar) {
+            System.out.println("Adding sugar...");
+            hasSugar = true;
+            System.out.println("Tea with sugar is ready.");
+        } else {
+            System.out.println("Cannot add sugar. Prepare basic tea first.");
+        }
+    }
+    
+    // Main method to execute the program
+    public static void main(String[] args) {
+        // Create instances of tea subclasses
+        Tea blackTea = new BlackTea();
+        Tea greenTea = new GreenTea();
+        Tea herbalTea = new HerbalTea();
+        
+        // Prepare different types of tea
+        System.out.println("Preparing black tea...");
+        blackTea.prepareTea();
+        System.out.println();
+        
+        System.out.println("Preparing green tea...");
+        greenTea.prepareTea();
+        System.out.println();
+        
+        System.out.println("Preparing herbal tea...");
+        herbalTea.prepareTea();
+    }
+}
+
+class BlackTea extends Tea {
     @Override
     public void prepareTea() {
         System.out.println("Preparing Black Tea...");
@@ -11,13 +81,11 @@ public class BlackTea extends Tea {
     }
     
     private void addTeaLeaves() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addTeaLeaves'");
+        System.out.println("Adding black tea leaves...");
     }
 
     private void boilWater() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'boilWater'");
+        System.out.println("Boiling water...");
     }
 
     // Method to steep black tea
@@ -26,8 +94,7 @@ public class BlackTea extends Tea {
     }
 }
 
-// GreenTea subclass
-public class GreenTea extends Tea {
+class GreenTea extends Tea {
     @Override
     public void prepareTea() {
         System.out.println("Preparing Green Tea...");
@@ -38,13 +105,11 @@ public class GreenTea extends Tea {
     }
     
     private void addTeaLeaves() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addTeaLeaves'");
+        System.out.println("Adding green tea leaves...");
     }
 
     private void boilWater() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'boilWater'");
+        System.out.println("Boiling water...");
     }
 
     // Method to steep green tea
@@ -53,8 +118,7 @@ public class GreenTea extends Tea {
     }
 }
 
-// HerbalTea subclass
-public class HerbalTea extends Tea {
+class HerbalTea extends Tea {
     @Override
     public void prepareTea() {
         System.out.println("Preparing Herbal Tea...");
@@ -65,8 +129,7 @@ public class HerbalTea extends Tea {
     }
     
     private void boilWater() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'boilWater'");
+        System.out.println("Boiling water...");
     }
 
     // Method to add herbs to herbal tea
@@ -78,5 +141,4 @@ public class HerbalTea extends Tea {
     private void steep() {
         System.out.println("Steeping herbal tea for 5 minutes.");
     }
-}
 }
